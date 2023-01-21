0256b31de52d2a06967cecaeb1332b89eb7dee84
levins@atonis <dev@atonis.io>
2023-01-21 12:14:42 +0530

    feat: header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,33 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      boxShadow:{
-        'light': '1px 1px 3px 0 rgba(0, 0, 0, .2)',
-      },
-      fontFamily: {
-        ibm: ['var(--font-ibm)', ...fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography')
-  ],
+		// Or if using `src` directory:
+		"./src/**/*.{ts,tsx}",
+	],
+	theme: {
+		extend: {
+			boxShadow: {
+				'light': '1px 1px 3px 0 rgba(0, 0, 0, .2)',
+			},
+			fontFamily: {
+				ibm: ['var(--font-ibm)', ...fontFamily.sans],
+			},
+			colors: {
+				'brand': {
+					600: '#df3940',
+				},
+				'sec-brand': {
+					600: '#f9c81b',
+				}
+			}
+		},
+	},
+	plugins: [
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography')
+	],
 }
