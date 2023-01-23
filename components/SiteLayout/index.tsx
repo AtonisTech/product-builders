59a7881fe9c9a08f138f518bd6fa38f61d4bf88f
levins@atonis <dev@atonis.io>
2023-01-23 13:48:06 +0530

    vercel build issue fix

--- components/SiteLayout/index.tsx
+++ components/SiteLayout/index.tsx
@@ -5,8 +5,8 @@ import { Pathway_Gothic_One } from "@next/font/google";
 import React from "react";
 
 
-const ibm_plex_sans = IBM_Plex_Sans({ weight: ['100', '200', '300', '400', '500', '600', '700'], variable: '--font-ibm', });
-const pathway_gothic_one = Pathway_Gothic_One({ weight: ['400'], variable: '--font-pathway' });
+const ibm_plex_sans = IBM_Plex_Sans({ weight: ['100', '200', '300', '400', '500', '600', '700'], subsets:['cyrillic', 'cyrillic-ext', 'greek', 'latin', 'latin-ext' , 'vietnamese'], variable: '--font-ibm', });
+const pathway_gothic_one = Pathway_Gothic_One({ weight: ['400'], subsets: ["latin" , "latin-ext"], variable: '--font-pathway' });
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
     return <div className={`contents ${ibm_plex_sans.variable} ${pathway_gothic_one.variable} font-ibm`}>
         <Header/>
